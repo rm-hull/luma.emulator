53e2060b81c7ffc5b394b56ac6a46aeb5c506381
Bryan <monty@english.net>
2020-03-24 23:20:55 -0400

    Handle the case if this test is run on windows - asciiblock not available on windows

--- tests/test_asciiblock.py
+++ tests/test_asciiblock.py
@@ -12,8 +12,6 @@ import struct
 import sys
 
 from luma.core.render import canvas
-from luma.emulator.device import asciiblock
-
 import baseline_data
 from helpers import patch, md5, redirect_stdout
 
@@ -22,44 +20,54 @@ def noop():
     pass
 
 
-def test_display():
-    scr_height = 40
-    scr_width = 100
-    fake_result = struct.pack('HHHH', scr_height, scr_width, 600, 616)
+try:
+    from luma.emulator.device import asciiblock
 
-    with redirect_stdout() as f:
-        sys.stdout.fileno = lambda: 1
-        with patch('fcntl.ioctl', return_value=fake_result):
-            device = asciiblock()
-            with canvas(device) as draw:
-                baseline_data.primitives(device, draw)
+    def test_display():
+        scr_height = 40
+        scr_width = 100
+        fake_result = struct.pack('HHHH', scr_height, scr_width, 600, 616)
 
-    device.cleanup = noop
-    out = f.getvalue()
+        with redirect_stdout() as f:
+            sys.stdout.fileno = lambda: 1
+            with patch('fcntl.ioctl', return_value=fake_result):
+                device = asciiblock()
+                with canvas(device) as draw:
+                    baseline_data.primitives(device, draw)
 
-    if sys.version_info > (3, 0):
-        out = out.encode('utf-8')
+        device.cleanup = noop
+        out = f.getvalue()
 
-    digest = hashlib.md5(out).hexdigest()
-    assert md5('tests/reference/asciiblock.txt') == digest
+        if sys.version_info > (3, 0):
+            out = out.encode('utf-8')
 
+        digest = hashlib.md5(out).hexdigest()
+        assert md5('tests/reference/asciiblock.txt') == digest
 
-def test_cleanup():
-    scr_height = 40
-    scr_width = 100
-    fake_result = struct.pack('HHHH', scr_height, scr_width, 600, 616)
+    def test_cleanup():
+        scr_height = 40
+        scr_width = 100
+        fake_result = struct.pack('HHHH', scr_height, scr_width, 600, 616)
 
-    with redirect_stdout() as f:
-        sys.stdout.fileno = lambda: 1
-        with patch('fcntl.ioctl', return_value=fake_result):
-            device = asciiblock()
-            device.cleanup()
+        with redirect_stdout() as f:
+            sys.stdout.fileno = lambda: 1
+            with patch('fcntl.ioctl', return_value=fake_result):
+                device = asciiblock()
+                device.cleanup()
 
-    device.cleanup = noop
-    out = f.getvalue()
+        device.cleanup = noop
+        out = f.getvalue()
 
-    if sys.version_info > (3, 0):
-        out = out.encode('utf-8')
+        if sys.version_info > (3, 0):
+            out = out.encode('utf-8')
 
-    digest = hashlib.md5(out).hexdigest()
-    assert digest == '3139690363a9edf4c03d553b36a37fe6'
+        digest = hashlib.md5(out).hexdigest()
+        assert digest == '3139690363a9edf4c03d553b36a37fe6'
+
+except ImportError:
+    def test_display():
+        # Not available on windows
+        pass
+
+    def test_cleanup():
+        pass
