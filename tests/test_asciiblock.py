6e79803574728ee317db635c0af76272e8bf0c5b
montgomeryb <monty@english.net>
2020-03-25 18:00:13 -0400

    Change to ignore packages not available on windows - allow pygame and… (#43)
    * Change to ignore packages not available on windows - allow pygame and image captures
    
    * Clean up to fix test and flake8
    
    * Handle the case if this test is run on windows - asciiblock not available on windows
    
    * Update reference gif - with prescibved color text
    
    * Some minor code refactoring

--- tests/test_asciiblock.py
+++ tests/test_asciiblock.py
@@ -12,8 +12,6 @@ import struct
 import sys
 
 from luma.core.render import canvas
-from luma.emulator.device import asciiblock
-
 import baseline_data
 from helpers import patch, md5, redirect_stdout
 
@@ -22,44 +20,55 @@ def noop():
     pass
 
 
+ASCII_AVAILABLE = True
+try:
+    from luma.emulator.device import asciiblock
+except ImportError:
+    ASCII_AVAILABLE = False
+
+
 def test_display():
-    scr_height = 40
-    scr_width = 100
-    fake_result = struct.pack('HHHH', scr_height, scr_width, 600, 616)
+    # If ascii art works, then do the test otherwise just end the function call
+    if ASCII_AVAILABLE:
+        scr_height = 40
+        scr_width = 100
+        fake_result = struct.pack('HHHH', scr_height, scr_width, 600, 616)
 
-    with redirect_stdout() as f:
-        sys.stdout.fileno = lambda: 1
-        with patch('fcntl.ioctl', return_value=fake_result):
-            device = asciiblock()
-            with canvas(device) as draw:
-                baseline_data.primitives(device, draw)
+        with redirect_stdout() as f:
+            sys.stdout.fileno = lambda: 1
+            with patch('fcntl.ioctl', return_value=fake_result):
+                device = asciiblock()
+                with canvas(device) as draw:
+                    baseline_data.primitives(device, draw)
 
-    device.cleanup = noop
-    out = f.getvalue()
+        device.cleanup = noop
+        out = f.getvalue()
 
-    if sys.version_info > (3, 0):
-        out = out.encode('utf-8')
+        if sys.version_info > (3, 0):
+            out = out.encode('utf-8')
 
-    digest = hashlib.md5(out).hexdigest()
-    assert md5('tests/reference/asciiblock.txt') == digest
+        digest = hashlib.md5(out).hexdigest()
+        assert md5('tests/reference/asciiblock.txt') == digest
 
 
 def test_cleanup():
-    scr_height = 40
-    scr_width = 100
-    fake_result = struct.pack('HHHH', scr_height, scr_width, 600, 616)
+    # If ascii art works, then do the test otherwise just end the function call
+    if ASCII_AVAILABLE:
+        scr_height = 40
+        scr_width = 100
+        fake_result = struct.pack('HHHH', scr_height, scr_width, 600, 616)
 
-    with redirect_stdout() as f:
-        sys.stdout.fileno = lambda: 1
-        with patch('fcntl.ioctl', return_value=fake_result):
-            device = asciiblock()
-            device.cleanup()
+        with redirect_stdout() as f:
+            sys.stdout.fileno = lambda: 1
+            with patch('fcntl.ioctl', return_value=fake_result):
+                device = asciiblock()
+                device.cleanup()
 
-    device.cleanup = noop
-    out = f.getvalue()
+        device.cleanup = noop
+        out = f.getvalue()
 
-    if sys.version_info > (3, 0):
-        out = out.encode('utf-8')
+        if sys.version_info > (3, 0):
+            out = out.encode('utf-8')
 
-    digest = hashlib.md5(out).hexdigest()
-    assert digest == '3139690363a9edf4c03d553b36a37fe6'
+        digest = hashlib.md5(out).hexdigest()
+        assert digest == '3139690363a9edf4c03d553b36a37fe6'
