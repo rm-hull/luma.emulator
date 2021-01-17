fa82bba2b57ab0a7a5814aab372b8ff35911eda2
Thijs Triemstra <info@collab.nl>
2021-01-17 17:09:19 +0100

    remove double paste

--- setup.py
+++ setup.py
@@ -5,10 +5,3 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup()
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
