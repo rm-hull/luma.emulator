4c819c606dc08f15e74b736d183ce53778375935
Thijs Triemstra <info@collab.nl>
2024-10-26 20:44:32 +0200

    bump version

--- luma/emulator/__init__.py
+++ luma/emulator/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2017-2023 Richard Hull and contributors
+# Copyright (c) 2017-2024 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-__version__ = '1.5.0'
+__version__ = '1.6.0'
